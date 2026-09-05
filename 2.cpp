d80d526a072dc62242e6d82a1bb4d762956e6671
kedarshrish <kedarshrish@gmail.com>
2026-09-05 19:56:59 +0530

    Added 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a = 123;
+
+    cout << a << endl;
+   
+    char b ='v';
+
+    cout << b <<endl;
+    
+    bool b1 = true;
+
+    cout << b1 << endl;
+
+    float f = 1.2;
+
+    cout << f << endl;
+
+    double d = 1.2345;
+
+    cout << d << endl;
+    
+}
