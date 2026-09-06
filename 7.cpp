7714f5d4c82053026d64651dedad08e1dfc438d1
kedarshrish <kedarshrish@gmail.com>
2026-09-06 23:35:27 +0530

    Added 7.cpp

---
+++ 7.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a,b;
+
+cout << "Enter a " << endl;
+cin >> a;
+
+cout << "Enetr b " << endl;
+cin >> b;
+
+if (a > b){
+
+    cout << " a > b " << endl;
+
+}
+
+if (b > a ){
+
+    cout << " b > a " << endl;
+
+
+}
+
+if (a == b ){
+
+    cout << "a = b " << endl;
+    
+} 
+
+
+}
