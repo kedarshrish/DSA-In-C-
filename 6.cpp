2aa421c72bc468c5625dd6176ff32a7581d28bf1
kedarshrish <kedarshrish@gmail.com>
2026-09-06 23:35:04 +0530

    Added 6.cpp

---
+++ 6.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    
+    if(n>0){
+   
+    cout << "No. is Positive " << endl;
+
+    }
+
+    else {
+   
+    cout << "No is Negative " << endl;
+
+    
+
+    }
+
+
+}
