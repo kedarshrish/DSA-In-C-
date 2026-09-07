87703b0a62a1e1d9c3dba2d7d85d31b5e3961ba2
kedarshrish <kedarshrish@gmail.com>
2026-09-07 19:49:16 +0530

    Added 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a = 9;
+
+if (a == 9){
+
+    cout << " Ninty " << endl;
+
+}
+
+ if (a > 0){
+
+    cout << "Positive" << endl;
+
+
+}
+
+else{
+
+    cout << "Negative" << endl;
+
+
+}
+
+}
