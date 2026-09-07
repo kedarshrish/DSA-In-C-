8c479685d7cb83be451d1fbdd18f69620bd0a9c9
kedarshrish <kedarshrish@gmail.com>
2026-09-07 19:49:44 +0530

    Added 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+    
+    int n;
+
+    cout << "Enetr n " << endl;
+    cin >> n;
+    int i = 1;
+    
+    while(i <= n){
+
+        cout << i <<" ";
+
+        i = i + 1 ;
+
+    }
+
+}
