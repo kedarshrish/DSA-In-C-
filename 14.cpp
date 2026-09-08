a6d71b4ba2e41a9610082ddbfcd76f01381aeb99
kedarshrish <kedarshrish@gmail.com>
2026-09-08 23:49:02 +0530

    Added 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i = 1;
+    while(i <= 3){
+
+
+            int j = 1;
+
+            while( j <= 3){
+
+                cout << i << " ";
+                j = j + 1;
+
+        
+            }
+
+            cout << endl;
+            i = i + 1;
+
+
+
+    }
+
+
+}
