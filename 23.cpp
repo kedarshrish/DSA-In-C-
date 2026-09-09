fad7f3054912f1291dd7e2be250c366e54df3e8d
kedarshrish <kedarshrish@gmail.com>
2026-09-09 22:56:17 +0530

    Added 23.cpp

---
+++ 23.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+
+    char ch = 'A';
+
+
+    int i = 1;
+    while( i <= 3){
+
+
+        int j = 1;
+        while( j <= 3){
+
+           cout << ch << " ";
+           ch = ch + 1;
+           j = j + 1;
+
+
+        }
+
+        i = i + 1;
+        cout << endl;
+        ch = 'A';
+        
+
+    }
+}
