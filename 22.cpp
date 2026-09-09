b6b1ad5370e5a1f793eaa99ca1a529b5509e5344
kedarshrish <kedarshrish@gmail.com>
2026-09-09 22:56:05 +0530

    Added 22.cpp

---
+++ 22.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    char ch = 65;
+   
+    int i = 1 ;
+    while( i <= 3){
+      
+         int j = 1;
+         while(j <= 3){
+ 
+           cout << ch << " ";
+           j = j + 1;
+
+         }
+
+         ch = ch + 1;
+         i = i + 1;
+         cout << endl;
+         
+
+    }
+
+}
