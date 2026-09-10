b36e18fd6e1d06dfbef20e783ab18fb2aee1f340
kedarshrish <kedarshrish@gmail.com>
2026-09-10 23:48:16 +0530

    Added 28.cpp

---
+++ 28.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char ch = 'D';
+
+
+    int i = 1;
+    while( i <= 4){
+      
+         int j = 1;
+         while(j <= i){
+
+            cout << ch << " ";
+            j = j + 1;
+            ch = ch +  1;
+
+
+         }
+
+         ch = 'D' - i;
+         i = i + 1;
+         cout << endl;
+
+    }
+}
