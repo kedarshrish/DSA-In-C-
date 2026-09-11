ec7cfcf23a94fa4e6b0cae09c718305a3e249d5f
kedarshrish <kedarshrish@gmail.com>
2026-09-11 22:03:33 +0530

    Added 33.cpp

---
+++ 33.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n = 1;
+    int i = 1;
+    while(i <= 4){
+  
+         int j = 1;
+         while(j <= 4-i){
+
+            cout << " ";
+            j = j + 1;
+
+
+         } 
+
+         j = 1;
+         while( j <= i){
+
+            cout << n ;
+            j = j + 1;
+
+
+         }
+
+         cout << endl;
+         i = i + 1;
+         n = n + 1;
+         
+
+
+    }
+}
