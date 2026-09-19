8a791c1b703188a51a8dc5e2405fe1a37ed9e9bd
kedarshrish <kedarshrish@gmail.com>
2026-09-19 23:27:20 +0530

    Added 40.cpp

---
+++ 40.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int n;
+  cout << "Enter n " << endl;
+  cin >> n;
+
+  cout << "numbers form 1 to n " << endl;
+
+  for(int i = 1 ; i <=n ; i++){
+
+    cout << i << endl;
+    
+    
+  }
+}
